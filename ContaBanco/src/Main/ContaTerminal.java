5f81327905920d1e70cf274cbd18d848dcf2f199
diegof856 <diegofelipe1025@gmail.com>
2024-05-04 18:02:14 -0300

    Corrigindo detalhes

--- ContaBanco/src/Main/ContaTerminal.java
+++ ContaBanco/src/Main/ContaTerminal.java
@@ -1,4 +1,5 @@
 package Main;
+
 import java.util.Scanner;
 
 import entidade.Conta;
@@ -9,34 +10,33 @@ public class ContaTerminal {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-		
-		//TODO: "inserir dados do cliente"
-		
-		Conta contaInserida = cadastarCliente(scanner);
-		
-		//imprimir dados
+
+		// TODO: "inserir dados do cliente"
+
+		Conta contaInserida = inserirCliente(scanner);
+
+		// imprimir dados
 		imprimirDados(contaInserida);
 		scanner.close();
 	}
-	
-	public static Conta cadastarCliente(Scanner scanner) {
+
+	public static Conta inserirCliente(Scanner scanner) {
 		System.out.print("Digite o seu nome: ");
 		String nome = scanner.nextLine();
 		System.out.print("Digite a agencia: ");
 		String agencia = scanner.nextLine();
 		System.out.print("Insira o numero da conta: ");
 		int numeroConta = scanner.nextInt();
-		System.out.println("Insira agora o saldo inicial: ");
+		System.out.print("Insira agora o saldo inicial: ");
 		double saldoInicial = scanner.nextDouble();
-		return new Conta(numeroConta, agencia, nome,saldoInicial);
+		return new Conta(numeroConta, agencia, nome, saldoInicial);
 	}
+
 	public static void imprimirDados(Conta contaInserida) {
-		System.out.println("Olá "+ contaInserida.getNomeCliente()+", obrigado por "
-				+ "criar uma conta em nosso banco, sua agência é "
-				+ contaInserida.getAgencia() + ", conta "
-				+ contaInserida.getNumero()+" e seu saldo "
-				+ contaInserida.getSaldo()+" já está disponível para saque");
+		System.out.println("Olá " + contaInserida.getNomeCliente() + ", obrigado por "
+				+ "criar uma conta em nosso banco, sua agência é " + contaInserida.getAgencia() + ", conta "
+				+ contaInserida.getNumero() + " e seu saldo " + contaInserida.getSaldo()
+				+ " já está disponível para saque");
 	}
-	
 
 }
